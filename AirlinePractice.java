9ffa8cfffc9c4c689958ac7174a01dafd6481fad
Syed Umar Ali Shah <umriechannel@gmail.com>
2026-02-17 19:25:45 +0500

    Update AirlinePractice.java

--- AirlinePractice.java
+++ AirlinePractice.java
@@ -6,7 +6,7 @@ public class AirlinePractice {
 
     public static boolean res(boolean[] seats, int start, int end, String type) {
         for (int i = start; i <= end; i++) {
-            if (!seats[i]) { // Using !seats[i] is cleaner than == false
+            if (!seats[i]) {
                 seats[i] = true;
                 System.out.printf("Success! Seat %d reserved in %s class.%n", i + 1, type);
                 return true;
@@ -20,7 +20,7 @@ public class AirlinePractice {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.println("\n--- Welcome to Umar's Airlines ---");
+            System.out.println("\n Welcome to Umar's Airlines");
             System.out.println("1. Economy (1-5)\n2. Business (6-10)\n0. Exit");
             System.out.print("Choice: ");
             int choice = sc.nextInt();
@@ -46,10 +46,10 @@ public class AirlinePractice {
                         System.out.print("Business full. Switch to Economy? (1 for Yes / 2 for No): ");
                         if (sc.nextInt() == 1) {
                             if (!res(seats, 0, 4, "Economy")) {
-                                System.out.println("Sorry, the entire flight is full. Next flight in 3 hrs.");
+                                System.out.println("Sorry, the entire flight is full. Next flight in 3 hrs");
                             }
                         } else {
-                            System.out.println("Next flight in 3 hrs.");
+                            System.out.println("Next flight in 3 hrs");
                         }
                     }
                     break;
